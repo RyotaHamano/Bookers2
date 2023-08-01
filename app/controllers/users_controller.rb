809b0b3567932dbe906191f2f002af37981dcab1
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-08-01 03:00:33 +0000

    [Start]課題4_モデル作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
   
   def index
     @user = User.find(current_user.id)
@@ -34,5 +36,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+   def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
-         
+
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name="guestuser"
+    end
+  end
+
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename:'default-image.jpg', content_type: 'image/jpeg')
-    end 
+    end
     profile_image.variant(resize_to_limit: [width,height]).processed
   end
-  
+
 end
